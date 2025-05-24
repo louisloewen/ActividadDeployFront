060e65d92ed88a799697a8871feb0e50ebbd02b8
Louis Loewen <144394298+louisloewen@users.noreply.github.com>
2025-05-23 20:33:00 -0600

    Arreglado4?

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import Welcome from "./components/Welcome";
 
 import useCount from "./hooks/useCount"; // o donde tengas definido useCount
 import { getItems } from "./api/items"; // o donde tengas definido getItems
-import ItemInfo from "./components/ItemInfo"; // componente que falta
+import ItemInfo from "./components/iteminfo"; // componente que falta
 
 
 function App() {
